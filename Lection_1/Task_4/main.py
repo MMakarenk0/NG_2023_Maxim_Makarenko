ae3a38ab9f5e2b59c9608868a100b3484970cdb5
Makarenko <makar44m@gmail.com>
2023-03-26 11:28:16 +0300

    Copy/paste removed

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -12,12 +12,12 @@ try:
     if sqrtdiscrim > 0:
         x1 = (-b - sqrtdiscrim)/2*a
         x2 = (-b + sqrtdiscrim)/2*a
-        print(f"Since the discriminant is greater than zero, the equation has 2 roots: {x1} and {x2}")
+        print(f"Discriminant is greater than zero, so the equation has 2 roots: {x1} and {x2}")
     elif sqrtdiscrim == 0:
         x1 = -b/2*a
-        print(f"Since the discriminant is zero, the equation has 1 root: {x1}")
+        print(f"Discriminant is zero, so the equation has 1 root: {x1}")
 except ValueError:
-    print("Since the discriminant is less than zero, the equation has no roots")
+    print("Discriminant is less than zero, so the equation has no roots")
 
 
 
