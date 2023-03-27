00aa40fc6023879384e0ca22f4c35567f9ea2d18
Makarenko <makar44m@gmail.com>
2023-03-27 17:46:11 +0300

    Added program for getting unique items

---
+++ Lection_2/Task_2/main.py
@@ -0,0 +1,3 @@
+print(set(input("Enter a sequence of elements separated by commas: ").split(", ")))
+# First of all, we takes user's string and split it 
+# After that we convert it to set to get unique items and display on the screen
