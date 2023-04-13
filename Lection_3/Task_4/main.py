a821255a2321ebc22d577287770189aee71608d5
Makarenko <makar44m@gmail.com>
2023-04-13 22:38:30 +0300

    Changed FAILED to FAIL

--- Lection_3/Task_4/main.py
+++ Lection_3/Task_4/main.py
@@ -9,7 +9,7 @@ def getStatusCode(linkList):
         elif response.status_code == 418:
             print(f"{response.status_code}: I'm a teapot")
         else:
-            print(f"{response.status_code}: FAILED")
+            print(f"{response.status_code}: FAIL")
 
 linkList = ["https://tasks.ngcourses.net/b/eLR8Hn6qpLx7WS6Nj/python-maxim-makarenko/juRv57w9y4NNAo3fs", 
             "https://www.youtube.com/", "https://www.google.com/teapot", "https://www.google.com/pagenotfound", 
