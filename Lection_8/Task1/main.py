8e65c172f645b09f548e528ecffb06d1bcb49372
Makarenko <makar44m@gmail.com>
2023-06-04 12:37:18 +0300

    Added task1 with argparse

--- Lection_8/Task1/main.py
+++ Lection_8/Task1/main.py
@@ -1,12 +1,13 @@
 import requests
 import re
 import threading
+import argparse
 
-def threadManager(threadNumber, imagesURL):
+def threadManager(threadNumber, imagesURL, path):
     
     threads = []
     for threadIndex in range(threadNumber):
-        threads.append(threading.Thread(target=downloadImages, args=(threadIndex, imagesURL[threadIndex::threadNumber])))
+        threads.append(threading.Thread(target=downloadImages, args=(threadIndex, imagesURL[threadIndex::threadNumber], path)))
         
     for thread in threads:
         thread.start()
@@ -24,23 +25,37 @@ def downloadImage(imageURL, savePath, threadIndex):
     else:
         print("url error")
 
-def downloadImages(threadIndex, imagesURL):
+def downloadImages(threadIndex, imagesURL, path):
     for index in range(len(imagesURL)):
         try:
-            downloadImage(imageURL=imagesURL[index], savePath=f'D:/downloaded_images/{str(threadIndex)+str(index)}.jpg', threadIndex=threadIndex)
+            downloadImage(imageURL=imagesURL[index], savePath=f'{path}/{str(threadIndex)+str(index)}.jpg', threadIndex=threadIndex)
         except:
             None
 
-url = 'https://uk.wikipedia.org/wiki/%D0%A2%D1%80%D0%B0%D0%BD%D0%B7%D0%B8%D1%81%D1%82%D0%BE%D1%80'
+def getImagesURLS(url):
+    response = requests.get(url)
+    html = response.text
 
-response = requests.get(url)
-html = response.text
+    imagesURL = re.findall(r'<img.+src="([^"]+)', html)
 
-imagesURL = re.findall(r'<img.+src="([^"]+)', html)
+    for index in range(len(imagesURL)):
+        if "https" not in imagesURL[index]:
+            imagesURL[index] = "https:" + imagesURL[index]
+    
+    return imagesURL
 
-for index in range(len(imagesURL)):
-    if "https" not in imagesURL[index]:
-        imagesURL[index] = "https:" + imagesURL[index]
 
-threadManager(threadNumber=4, imagesURL=imagesURL)
+parser = argparse.ArgumentParser()
+    
+
+parser.add_argument('--url', required=True, help='Link of the site from which we will download the images.')
+parser.add_argument('--path', required=True, help='The folder to save downloaded pictures.')
+parser.add_argument('--threads', required=False, default=4, help='(optional)The number of threads for programme(WARNING! Do not use very large number! Default number: 4)')
+
+
+args = parser.parse_args()
+
+
+
+threadManager(threadNumber=args.threads, imagesURL=getImagesURLS(args.url), path=args.path)
 
